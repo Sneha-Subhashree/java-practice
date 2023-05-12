72de566dccd8af02e73d23c9e36c1236f00a2444
Sneha-Subhashree <98444132+Sneha-Subhashree@users.noreply.github.com>
2023-05-12 21:56:58 +0530

    take input and print on screen

---
+++ java stdin and stdout.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        // Complete this line
+        // String name = scan.next();
+        // Complete this line
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        // Complete this line
+        // System.out.println( " name is " + name);
+        // Complete this line
+    }
+}
